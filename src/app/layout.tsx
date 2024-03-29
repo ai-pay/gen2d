d800c2f50fc1273db0ca87a0acb943249e00cf45
Marchusness <m_gunny31@outlook.com>
2024-03-30 08:13:53 +1030

    add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "../lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, "bg-neutral-50")}>{children}</body>
+      <Analytics />
     </html>
   );
 }
