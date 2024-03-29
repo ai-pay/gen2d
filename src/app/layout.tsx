b3b331d5fcb94c4d8cdefc05a6f9815094b25942
Marchusness <m_gunny31@outlook.com>
2024-03-29 20:54:43 +1030

    this might work?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter } from "@next/font/google";
 import "./globals.css";
 import { cn } from "../lib/utils";
 
