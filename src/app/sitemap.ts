8a5037957c6aa903f0809699577fb0aef4247d32
Marchusness <m_gunny31@outlook.com>
2024-04-04 03:40:37 +1030

    add dynamic sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,31 @@
+import { RECENT_IMAGE_IDS_KEY } from "@/database/redis/recentImageIds/key";
+import { redis } from "@/database/redis/redisClient";
+import { MetadataRoute } from "next";
+ 
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const urls = await redis.get(RECENT_IMAGE_IDS_KEY) as string[] ?? [];
+
+  const dynamic = urls.map((url) => ({
+    url: `https://www.gen2d.dev/image/${url}`,
+    lastModified: new Date(),
+    changeFrequency: "yearly" as const,
+    priority: 0.9,
+  }));
+    
+
+  return [
+    {
+      url: "https://www.gen2d.dev",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.gen2d.dev/dev",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+    ...dynamic,
+  ];
+}
