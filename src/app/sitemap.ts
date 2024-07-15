4754e6889fde8d1a50376d8188d3d1fa366c0895
Marchusness <m_gunny31@outlook.com>
2024-07-15 21:49:14 +0930

    bruh sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,17 +1,16 @@
+import { MetadataRoute } from "next";
 import { RECENT_IMAGE_IDS_KEY } from "@/database/redis/recentImageIds/key";
 import { redis } from "@/database/redis/redisClient";
-import { MetadataRoute } from "next";
- 
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const urls = await redis.lrange(RECENT_IMAGE_IDS_KEY, 0, -1) as string[] ?? [];
 
   const dynamic = urls.map((url) => ({
-    url: `https://www.gen2d.dev/image/${url}`,
+    url: `https://www.gen2d.dev/img/${url}`,
     lastModified: new Date(),
     changeFrequency: "yearly" as const,
     priority: 0.9,
   }));
-    
 
   return [
     {
